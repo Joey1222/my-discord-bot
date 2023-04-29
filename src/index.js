474201ecfefb72e4e11e7d72ce2d80870467ad51
jmknu <101217373+Joey1222@users.noreply.github.com>
2023-04-29 14:00:58 -0500

    base

---
+++ src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { Client, IntentsBitField, messageLink } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.on('ready', (c) => {
+    console.log(`✅ ${c.user.tag} is online.`)
+});
+
+client.on('messageCreate', (msg) => {
+    if (msg.author.bot) {
+        return;
+    }
+
+    if (msg.content === 'hello') {
+        msg.reply('Hey!')
+    }
+})
+
+client.login(process.env.TOKEN);
